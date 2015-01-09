c6f9a98703fedcdc9a40d9d10ae37a5a256f76a7
Xiaofei Fu <7880774@qq.com>
2015-01-09 14:18:59 +0800

    代码修复

--- web.R
+++ web.R
@@ -2,11 +2,13 @@
 library(rvest)
 url = 'http://www.tianqi.com/air/'
 
-d1 = url %>% html_session() %>% html_nodes("li .td-1st") %>% html_text() 
-d2 = url %>% html_session() %>% html_nodes(".td-2nd a[href*='air']") %>%  html_text()
-qu = url %>% html_session() %>% html_nodes(".td-4rd") %>% html_text()
+d1 = url %>% html_session() %>% html_nodes("li .td-1st") %>% html_text(encoding="UTF-8") 
+d2 = url %>% html_session() %>% html_nodes(".td-2nd a[href*='air']") %>%  html_text(encoding="UTF-8")
+qu = url %>% html_session() %>% html_nodes(".td-4rd") %>% html_text(encoding="UTF-8")
 d3 = qu[c(seq(1,length(qu),2))]
-d4 = url %>% html_session() %>% html_nodes(".td-4rd em") %>% html_text() 
+d4 = url %>% html_session() %>% html_nodes(".td-4rd em") %>% html_text(encoding="UTF-8") 
 
 data = data.frame(排名 = d1,城市 = d2,空气质量指数 = d3,空气质量 = d4)
+
+head(data)
 #附html_nodes函数里面用到的CSS selector的语法网址： http://www.w3.org/TR/css3-selectors/
